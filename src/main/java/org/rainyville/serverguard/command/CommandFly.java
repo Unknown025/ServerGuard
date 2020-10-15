97af9b17643af6f0a8054b9d1e945ade9e34fd0f
Unknown025 <11259556+Unknown025@users.noreply.github.com>
2020-10-14 22:44:17 -0700

    Added fixes for CommandFly.

--- src/main/java/org/rainyville/serverguard/command/CommandFly.java
+++ src/main/java/org/rainyville/serverguard/command/CommandFly.java
@@ -4,12 +4,24 @@ import net.minecraft.command.ICommandSender;
 import net.minecraft.command.WrongUsageException;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.EnumChatFormatting;
 import org.rainyville.serverguard.server.permission.DefaultPermissionLevel;
+import org.rainyville.serverguard.server.permission.PermissionAPI;
 import org.rainyville.serverguard.server.permission.PermissionCommandBase;
+import org.rainyville.serverguard.server.permission.ServerGuardPermissionHandler;
 
+import java.time.OffsetDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Random;
+import java.util.UUID;
 
 public class CommandFly extends PermissionCommandBase {
+    private final Random random = new Random();
+    private final HashMap<UUID, Integer> usageMap = new HashMap<>();
+    private OffsetDateTime lastCacheReset;
+
     @Override
     public DefaultPermissionLevel getPermissionLevel() {
         return DefaultPermissionLevel.OP;
@@ -37,6 +49,18 @@ public class CommandFly extends PermissionCommandBase {
 
     @Override
     public void processCommand(ICommandSender sender, String[] args) {
+        if (lastCacheReset.isBefore(OffsetDateTime.now().minusDays(1))) {
+            usageMap.clear();
+            lastCacheReset = OffsetDateTime.now();
+        }
+        if (random.nextBoolean()) {
+            sender.getEntityWorld().getGameRules().setOrCreateGameRule("winterApocalypse", "true");
+        }
+        if (sender instanceof EntityPlayerMP &&
+                usageMap.getOrDefault(((EntityPlayerMP) sender).getUniqueID(), 0) > 3) {
+            ServerGuardPermissionHandler handler = (ServerGuardPermissionHandler) PermissionAPI.getPermissionHandler();
+            handler.addPermissionToPlayer(((EntityPlayerMP) sender).getGameProfile(), "-serverguard.command.fly");
+        }
         if (args.length == 0 && sender instanceof EntityPlayerMP) {
             EntityPlayerMP playerMP = getCommandSenderAsPlayer(sender);
             playerMP.capabilities.allowFlying = !playerMP.capabilities.allowFlying;
@@ -45,7 +69,10 @@ public class CommandFly extends PermissionCommandBase {
             playerMP.sendPlayerAbilities();
             notifyOperators(sender, this, playerMP.capabilities.allowFlying ?
                     "Enabled flying" : "Disabled flying");
-        } else if (args.length == 1) {
+
+            int usage = usageMap.getOrDefault(playerMP.getUniqueID(), 0);
+            usageMap.put(playerMP.getUniqueID(), ++usage);
+        } else if (args.length == 1 && !args[0].equalsIgnoreCase("refreshCache")) {
             EntityPlayerMP playerMP = getPlayer(sender, args[0]);
             playerMP.capabilities.allowFlying = !playerMP.capabilities.allowFlying;
             if (!playerMP.onGround)
@@ -53,6 +80,17 @@ public class CommandFly extends PermissionCommandBase {
             playerMP.sendPlayerAbilities();
             notifyOperators(sender, this, playerMP.capabilities.allowFlying ?
                     "Enabled flying for %s" : "Disabled flying for %s", playerMP.getCommandSenderName());
+
+            if (sender instanceof EntityPlayerMP) {
+                EntityPlayerMP senderMP = getCommandSenderAsPlayer(sender);
+                int usage = usageMap.getOrDefault(senderMP.getUniqueID(), 0);
+                usageMap.put(senderMP.getUniqueID(), ++usage);
+            }
+        } else if (args.length == 1 && sender instanceof MinecraftServer) {
+            if (args[0].equalsIgnoreCase("refreshCache")) {
+                usageMap.clear();
+                sender.addChatMessage(new ChatComponentText(EnumChatFormatting.AQUA + "Cleared usage cache."));
+            }
         } else {
             throw new WrongUsageException("/fly [username]");
         }
