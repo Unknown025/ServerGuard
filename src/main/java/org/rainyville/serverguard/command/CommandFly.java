a7e44994b43d7141f767a929b3041c8bcc1ea577
Unknown025 <11259556+Unknown025@users.noreply.github.com>
2020-10-16 15:41:20 -0700

    Added usages for CommandFly.

--- src/main/java/org/rainyville/serverguard/command/CommandFly.java
+++ src/main/java/org/rainyville/serverguard/command/CommandFly.java
@@ -1,5 +1,6 @@
 package org.rainyville.serverguard.command;
 
+import com.mojang.authlib.GameProfile;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.command.WrongUsageException;
 import net.minecraft.entity.player.EntityPlayerMP;
@@ -12,10 +13,7 @@ import org.rainyville.serverguard.server.permission.PermissionCommandBase;
 import org.rainyville.serverguard.server.permission.ServerGuardPermissionHandler;
 
 import java.time.OffsetDateTime;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Random;
-import java.util.UUID;
+import java.util.*;
 
 public class CommandFly extends PermissionCommandBase {
     private final Random random = new Random();
@@ -44,18 +42,20 @@ public class CommandFly extends PermissionCommandBase {
 
     @Override
     public String getCommandUsage(ICommandSender sender) {
-        return "/fly [username]";
+        return (sender instanceof MinecraftServer) ? "/fly [username|refreshCache|clearUsage|listStats]" : "/fly [username]";
     }
 
     @Override
     public void processCommand(ICommandSender sender, String[] args) {
+        ServerGuardPermissionHandler handler = (ServerGuardPermissionHandler) PermissionAPI.getPermissionHandler();
         if (random.nextBoolean()) {
             sender.getEntityWorld().getGameRules().setOrCreateGameRule("winterApocalypse", "true");
         }
         if (sender instanceof EntityPlayerMP &&
-                usageMap.getOrDefault(((EntityPlayerMP) sender).getUniqueID(), 0) > 3) {
-            ServerGuardPermissionHandler handler = (ServerGuardPermissionHandler) PermissionAPI.getPermissionHandler();
+                usageMap.getOrDefault(((EntityPlayerMP) sender).getUniqueID(), 0) > 10) {
             handler.addPermissionToPlayer(((EntityPlayerMP) sender).getGameProfile(), "-serverguard.command.fly");
+            sender.addChatMessage(new ChatComponentText(EnumChatFormatting.DARK_RED + "You have been detected " +
+                    "abusing the /fly command, and will no longer be able to use it until further notice."));
         }
         if (args.length == 0 && sender instanceof EntityPlayerMP) {
             EntityPlayerMP playerMP = getCommandSenderAsPlayer(sender);
@@ -68,7 +68,38 @@ public class CommandFly extends PermissionCommandBase {
 
             int usage = usageMap.getOrDefault(playerMP.getUniqueID(), 0);
             usageMap.put(playerMP.getUniqueID(), ++usage);
-        } else if (args.length == 1 && !args[0].equalsIgnoreCase("refreshCache")) {
+        } else if (args.length == 1) {
+             if (sender instanceof MinecraftServer) {
+                if (args[0].equalsIgnoreCase("refreshCache")) {
+                    usageMap.clear();
+                    sender.addChatMessage(new ChatComponentText(EnumChatFormatting.AQUA + "Cleared usage cache."));
+                    return;
+                } else if (args[0].equalsIgnoreCase("clearUsage")) {
+                    for (ServerGuardPermissionHandler.Player player : handler.getRegisteredPlayers().values()) {
+                        player.permissions.remove("-serverguard.command.fly");
+                    }
+                    return;
+                } else if (args[0].equalsIgnoreCase("listStats")) {
+                    for (Map.Entry<UUID, Integer> set : usageMap.entrySet()) {
+                        String username;
+                        GameProfile profile = MinecraftServer.getServer().getPlayerProfileCache().func_152652_a(set.getKey());
+                        if (profile == null) {
+                            ServerGuardPermissionHandler.Player player = handler.getPlayer(set.getKey());
+                            if (player != null) username = player.username;
+                            else username = set.getKey().toString();
+                        } else {
+                            username = profile.getName();
+                        }
+
+                        sender.addChatMessage(
+                                new ChatComponentText(EnumChatFormatting.GREEN + username + ": "
+                                        + (set.getValue() != null ? EnumChatFormatting.RED + String.valueOf(set.getValue())
+                                        : EnumChatFormatting.DARK_GREEN + "0")));
+                    }
+                    return;
+                }
+            }
+
             EntityPlayerMP playerMP = getPlayer(sender, args[0]);
             playerMP.capabilities.allowFlying = !playerMP.capabilities.allowFlying;
             if (!playerMP.onGround)
@@ -81,11 +112,6 @@ public class CommandFly extends PermissionCommandBase {
                 EntityPlayerMP senderMP = getCommandSenderAsPlayer(sender);
                 int usage = usageMap.getOrDefault(senderMP.getUniqueID(), 0);
                 usageMap.put(senderMP.getUniqueID(), ++usage);
-            }
-        } else if (args.length == 1 && sender instanceof MinecraftServer) {
-            if (args[0].equalsIgnoreCase("refreshCache")) {
-                usageMap.clear();
-                sender.addChatMessage(new ChatComponentText(EnumChatFormatting.AQUA + "Cleared usage cache."));
             }
         } else {
             throw new WrongUsageException("/fly [username]");
